4b61f8d06120cb845ba1ce3a468d52e27c2211a1
Madhav Paudel <poudelmadhav143@gmail.com>
2021-04-02 19:24:43 +0545

    first graph query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,10 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :items, [Types::ItemType], null: false,
+      description: "Returns a list of items in the martin libraty"
+    def items
+      Item.all
     end
   end
 end
